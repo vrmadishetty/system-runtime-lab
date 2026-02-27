e716a32c798c997a0fd30d6cc3312bff88643948
vrmadishetty <ramana.madishetty2805@gmail.com>
2026-02-27 00:35:02 +0000

    riscv: jump from assembly to C
    Precondition is sp has to be usable stack address.

--- risc-v/m.s
+++ risc-v/m.s
@@ -1,5 +1,6 @@
 #write a loop that add two number repeatedly.
 _start:
-    j ramana
+    li sp, 0x80002000 #load the stack pointer with a reasonable value. The value that is reachable by the CPU
+    jal ramana # 
 j .    
     
