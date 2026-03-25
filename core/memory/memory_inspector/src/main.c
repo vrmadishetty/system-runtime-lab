0cf5e49063a912cd483f7e17d7e3680d2b30502a
vrmadishetty <ramana.madishetty2805@gmail.com>
2026-03-25 00:19:11 +0000

    experiment: sensor management

--- core/memory/memory_inspector/src/main.c
+++ core/memory/memory_inspector/src/main.c
@@ -1,40 +1,129 @@
 #include <stdio.h>
-#include <math.h>
 
+enum sensor_type {
+    TEMPERATURE,
+    HUMIDITY,
+    PRESSURE
+};
 
-int main(){
-    float samples_per_second = 44100;
-    int tone_freq = 440;
-    int duration = 2;
-    int max_value_encoding = 32767;
+enum sensor_state {
+    ACTIVE,
+    INACTIVE,
+    ERROR
+};
 
-    int i = 0;
-    int total_samples = duration * samples_per_second;
+struct temperature_data {
+    short int min;
+    short int max;
+    float reading;
+} __attribute__((packed));
 
-    float time = 0;
-    float angle;
+struct humidity_data {
+    float calibration;
+    float reading;
+}__attribute__((packed));;
 
-    short int sample;
+struct pressure_data {
+    short int altitude;
+    float reading;
+}__attribute__((packed));;
 
-    FILE *f;
-    f = fopen("sine.raw", "wb");
-    if (f == NULL) {
-        printf("could not open a file. \n");
-        return -1;
+union readings {
+    struct temperature_data temp;
+    struct humidity_data humidity;
+    struct pressure_data pressure;
+};
+
+struct sensor {
+    unsigned char id;
+    char name[20];
+    enum sensor_type type;
+    enum sensor_state status;
+    union readings value;
+}__attribute__((packed));
+
+int main ()
+{
+    struct sensor sensor_list[10] = {0};
+    struct sensor tmp = {0};
+    unsigned int add_sensor;
+    unsigned int sensor_count = 0;
+    unsigned int init_sensor = 0;
+
+    while (1) {
+        if (sensor_count >= 10) {
+            printf("No slots add more sensors\n");
+        }
+        init_sensor = 0;
+        printf("Want to add sensor? Enter 1 to add, 0 to skip: ");
+        scanf("%u", &init_sensor);
+
+        if(init_sensor == 0) {
+            break;
+        }
+
+        printf("ID: ");
+        scanf("%c", &tmp.id);
+        scanf("%*c");
+
+        printf("Name: ");
+        scanf("%s", (char *)&tmp.name);
+        scanf("%*c");
+
+        printf("Type (t, h, p): ");
+        char type;
+        scanf("%c", &type);
+        scanf("%*c");
+
+        switch (type) {
+            case 't': tmp.type = TEMPERATURE;
+                break;
+            case 'h': tmp.type = HUMIDITY;
+                break;
+            case 'p': tmp.type = PRESSURE;
+                break;
+        }
+        tmp.status = INACTIVE;
+
+        switch (tmp.type) {
+            case TEMPERATURE:
+                printf("min: ");
+                scanf("%hd", &tmp.value.temp.min);
+                scanf("%*c");
+                
+                printf("max: ");
+                scanf("%hd", &tmp.value.temp.max);
+                scanf("%*c");
+
+                printf("reading: ");
+                scanf("%f", &tmp.value.temp.reading);
+                scanf("%*c");
+            break;
+            case HUMIDITY:
+                printf("calibration: ");
+                scanf("%f", &tmp.value.humidity.calibration);
+                scanf("%*c");
+
+                printf("reading: ");
+                scanf("%f", &tmp.value.humidity.reading);
+                scanf("%*c");
+            break;
+            case PRESSURE:
+                printf("altitude: ");
+                scanf("%hd", &tmp.value.pressure.altitude);
+                scanf("%*c");
+
+                printf("reading: ");
+                scanf("%f", &tmp.value.pressure.reading);
+                scanf("%*c");
+            break;
+        }
+        break;
+
     }
+    printf("ID: %u\n", tmp.id);
+    printf("Name: %s\n", tmp.name);
+    printf("Type: %u\n", tmp.type);
 
-    while (i < total_samples){
-        time = i * (1/samples_per_second);
-        angle = 2 * 3.14 * tone_freq * time;
-
-        sample = max_value_encoding * sin(angle);
-
-        fwrite(&sample, 2, 1, f);
-        printf("%d ", sample);
-        i = i + 1; 
-    }
-
-    fclose(f);
     return 0;
 }
-
